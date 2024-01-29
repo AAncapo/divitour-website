0ce374fcf5a9c11d9a67b61655e59de61ed9f42d
AAncapo <raidellg9511@gmail.com>
2024-01-29 17:11:17 -0500

    update

--- script/app.js
+++ script/app.js
@@ -1,6 +1,48 @@
+// (in HTML) all skip-buttons must be "connected" to a .image-slideshow with the prop "id"=(the skip-button's "name")
+let currIndex = 0;
+
+let sliderImages = [
+  "images/destinos/varadero/royalton-hicacos/varadero-hotel-royalton-hicacos.jpeg",
+  "images/destinos/varadero/royalton-hicacos/varadero-hotel-royalton-hicacos1.jpeg",
+  "images/destinos/varadero/royalton-hicacos/varadero-hotel-royalton-hicacos2.jpeg",
+];
+connectSkipButtons();
+//setSlideshowImage(currIndex);
+
+function connectSkipButtons() {
+  const skipButtons = document.querySelectorAll(".skip-button");
+  const imgSshows = document.querySelectorAll(".image-slideshow");
+
+  //loop thru all image-slideshows and for each one loop thru all the skipbuttons to find their partner
+  imgSshows.forEach((iss) => {
+    skipButtons.forEach((sb) => {
+      if (sb.name == iss.id) {
+        sb.addEventListener("click", function (e) {
+          let act;
+          sb.className == "btn-left" ? (act = "prev") : (act = "next");
+          setSlideshowImage(iss, act);
+        });
+      }
+    });
+  });
+}
+
+function setSlideshowImage(container, action) {
+  // Animate images
+  currIndex += 1 ? action == "next" : (currIndex -= 1);
+  if (currIndex > sliderImages.length - 1) {
+    currIndex = 0;
+  } else if (currIndex < 0) {
+    currIndex = sliderImages.length;
+  }
+  console.log(currIndex);
+  container.setAttribute("src", sliderImages[currIndex]);
+}
+
 const accountButton = document.getElementById("account-btn");
 const accountTools = document.querySelector(".account-tools");
 
+// Toggle Account button ((TODO: buttons have a toggle prop, dummy))
 let isAccountToolsVisible = false;
 accountButton.addEventListener("click", function (e) {
   if (!isAccountToolsVisible) {
@@ -11,16 +53,6 @@ accountButton.addEventListener("click", function (e) {
     isAccountToolsVisible = false;
   }
 });
-
-//Parallax effect (descartado)
-/*window.addEventListener("scroll", function (e) {
-  //will not work on IE 9
-  var scrolled = this.window.scrollY;
-  //will not work on IE 8
-  const rocks = this.document.querySelector(".novedades-section");
-  rocks.style.top = -(scrolled * 0.2) + "px";
-});
-*/
 
 let index = 0;
 //displayImages();
@@ -37,3 +69,13 @@ function displayImages() {
   images[index - 1].style.display = "block";
   setTimeout(displayImages, 5000);
 }
+
+//Parallax effect (descartado)
+/*window.addEventListener("scroll", function (e) {
+  //will not work on IE 9
+  var scrolled = this.window.scrollY;
+  //will not work on IE 8
+  const rocks = this.document.querySelector(".novedades-section");
+  rocks.style.top = -(scrolled * 0.2) + "px";
+});
+*/
