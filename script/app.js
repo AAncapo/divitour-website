9bd8f664e3a2cc7c4ed75345160b3209e302ea1f
AAncapo <raidellg9511@gmail.com>
2024-02-05 10:43:17 -0500

    update event image

--- script/app.js
+++ script/app.js
@@ -1,11 +1,14 @@
 const popDestContainer = document.querySelector(".pop-destinations");
 
-setDestinations("images/destinos/varadero/varadero2.jpeg", "Varadero");
-setDestinations(
-  "images/destinos/cayo-largo/cayolargo1.png",
-  "Cayo Largo del Sur"
-);
+setDestinations("images/destinos/varadero2.jpeg", "Varadero");
+setDestinations("images/destinos/cayolargo1.png", "Cayo Largo del Sur");
 setDestinations("images/destinos/jibacoa.JPG", "Jibacoa");
+setDestinations("images/destinos/havana.JPG", "Havana");
+setDestinations("", "Santiago de Cuba");
+setDestinations("", "Cayo Levisa");
+setDestinations("", "Peninsula de Zapata");
+setDestinations("", "Santa Lucia");
+setDestinations("", "Maria la Gorda");
 
 function setDestinations(imgPath, dstName) {
   let destContainerDiv = document.createElement("div");
@@ -16,12 +19,17 @@ function setDestinations(imgPath, dstName) {
   destContainerDiv.appendChild(destImageDiv);
 
   let _img = document.createElement("img");
-  _img.src = imgPath;
+  if (imgPath === "") {
+    _img.style.backgroundColor = "#fff";
+  } else {
+    _img.src = imgPath;
+  }
   destImageDiv.appendChild(_img);
 
+  // add gradient image
   let destTextBgDiv = document.createElement("div");
   destTextBgDiv.className = "dest-text-bg";
-  destContainerDiv.appendChild(destTextBgDiv);
+  destImageDiv.appendChild(destTextBgDiv);
 
   // name
   let destNameH = document.createElement("h3");
