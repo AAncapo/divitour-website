635a1c33ab2c2612df3bb265207861f610513292
AAncapo <raidellg9511@gmail.com>
2024-03-05 13:35:23 -0500

    footer.inc fixed

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,42 +1,39 @@
-<footer class="container-fluid bg-dark" style="height: 600px;">
-    <!-- Contacts -->
-      <div id="contacts" class="container d-flex" style="margin-bottom: 100px;flex-direction:column">
-        <h3 class="text-light text-center mt-5 section-title">Contact Us</h3>
-        <div class="contact-items">
-          <span class="contact-item">
-            <img src=<?php echo $base_url . 'images/icons/whatsapp.png'; ?> /><a
-              href="https://wa.me/5352529974" target="_blank"
-              >(+53) 52529974</a
-            >
-          </span>
-          <span class="contact-item">
-            <img src=<?php echo $base_url . 'images/icons/icons8-address.png'; ?> /><a
-              style="text-decoration: none">5ta Avenida e/ 62 & 66, Miramar, La Habana, Cuba</a>
-          </span>
-          <span class="contact-item">
-            <img src=<?php echo $base_url . 'images/icons/icons8-mail.png'; ?> /><a
-              href="mailto: info@divitour.com"
-              target="_blank"
-              >info@divitour.com</a
-            >
-          </span>
-          <!-- Social Media -->
-          <div class="social-media">
-            <span class="contact-item">
-              <a href="https://www.facebook.com/divitourtravel/" target="_blank"
-              ><img src=<?php echo $base_url . 'images/icons/icons8-facebook.png'; ?> /></a>
-            </span>
-            <span class="contact-item">
-              <a href="https://www.instagram.com/divitourtravel/" target="_blank"
-              ><img src=<?php echo $base_url . 'images/icons/icons8-instagram.png'; ?> /></a>
-            </span>
-          </div>
+<footer class="container-fluid bg-dark">
+
+  <!-- Contacts -->
+  <div id="contacts" class="container mb-5 py-5">
+    <h3 class="text-light text-center py-2 section-title">Contact Us</h3>
+
+    <div class="container-fluid">
+      <!-- Contact Links -->
+      <div class="d-flex flex-sm-row flex-column justify-content-center align-items-center gap-5 mb-2">
+        <div class="col-3 contactItem">
+          <a class=" text-decoration-none text-light" href="https://wa.me/5352529974" target="_blank"><img src=<?php echo $base_url . 'images/icons/whatsapp.png'; ?> />  (+53) 52529974</a>
+        </div>
+        <div class="col-6 contactItem">
+          <a class=" text-decoration-none text-light"><img src=<?php echo $base_url . 'images/icons/icons8-address.png'; ?> />  5ta Avenida e/ 62 & 66, Miramar, La Habana, Cuba</a>
+        </div>
+        <div class="col-3 contactItem">
+          <a class=" text-decoration-none text-light" href="mailto: info@divitour.com" target="_blank"><img src=<?php echo $base_url . 'images/icons/icons8-mail.png'; ?> />  info@divitour.com</a>
         </div>
       </div>
+
+      <!-- Social Media -->
+      <span class="d-flex justify-content-center gap-3">
+        <a class="contactItem" href="https://www.facebook.com/divitourtravel/" target="_blank">
+          <img src=<?php echo $base_url . 'images/icons/icons8-facebook.png'; ?> />
+        </a>
+        <a class="contactItem" href="https://www.instagram.com/divitourtravel/" target="_blank">
+          <img src=<?php echo $base_url . 'images/icons/icons8-instagram.png'; ?> />
+        </a>
+      </span>
+    </div>
+  </div>
+
     <!-- Partners -->
-    <div class="container mb-5">
-      <h3 class="text-center text-light mt-5 section-title">Our Partners</h3>
-      <div class="row mb-5 w-100 partners-container">
+    <div class="container pb-5">
+      <!-- <h3 class="text-center text-light mt-5 section-title">Our Partners</h3> -->
+      <div class="row w-100 partners-container">
         <a class="col partner" href="/">
           <img
             src=<?php echo $base_url . 'images/partners/cammproductions.png'; ?>
@@ -84,7 +81,6 @@
       integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
       crossorigin="anonymous"
     ></script>
-
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
       integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
