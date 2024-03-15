bc9bfaed5e3a82a3cb612b3b19ce071b1dcd371c
AAncapo <raidellg9511@gmail.com>
2024-03-15 14:07:58 -0400

    adjusted icons alignment in servcard

--- includes/elements/servcard.inc.php
+++ includes/elements/servcard.inc.php
@@ -12,14 +12,14 @@
             <div class="card-body">
                 <h5 id="servName" class="card-title"><?php echo $row['nombre'] ?></h5>
                 <p id="servDesc" class="card-text collapsable-text"><?php echo substr($row['descripcion'], 0, strpos($row['descripcion'],'.')) ?></p>
-                <div class="d-flex justify-content-between align-items-center mb-3">
+                <div class="d-flex justify-content-start align-items-center mb-3">
                 <!-- Cost -->
                 <?php
                 $base_cost = $row['base_cost'];
                 // $over_cost = $row['over_cost'];
                 if ($base_cost && $base_cost > 0) { ?>
-                <div class="col d-block d-md-flex flex-md-column gap-1 align-items-center">
-                    <div  class="m-auto m-md-0" style="width: 45px; height:45px;">
+                <div class="col col-md-2 d-block d-md-flex flex-md-column gap-1 align-items-center">
+                    <div class="m-auto m-md-0" style="width: 45px; height:45px;">
                     <img class="w-100 h-100 object-fit-contain" src="../images/icons/money-dollar-svgrepo-com.svg">
                     </div>
                     <div class="d-flex d-md-block">
@@ -40,7 +40,7 @@
                 <?php 
                 $max_pax = explode(",",$row['pax_range'])[1];
                 if ($max_pax > 0) { ?>
-                <div class="col d-block d-md-flex flex-md-column gap-1 align-items-center">
+                <div class="col col-md-2 d-block d-md-flex flex-md-column gap-1 align-items-center">
                     <div class="m-auto m-md-0" style="width: 35px; height: 35px;">
                     <img class="w-100 h-100 object-fit-contain" src="../images/icons/person-explaining-something-svgrepo-com.svg">
                     </div>
@@ -55,7 +55,7 @@
                 <?php 
                 $duration = $row['duracion']; 
                 if ($duration && $duration > 0) { ?>
-                <div class="col d-block d-md-flex flex-md-column gap-1 align-items-center">
+                <div class="col col-md-2 d-block d-md-flex flex-md-column gap-1 align-items-center">
                     <div class="m-auto m-md-0" style="width: 35px; height: 35px;">
                     <img class="w-100 h-100 object-fit-contain" src="../images/icons/time-svgrepo-com.svg">
                     </div>
